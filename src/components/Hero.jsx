ef5fdb7fa00f79b5f96792d6bd9a92994e09a4ed
Astromify <oscar.lennermark@gmail.com>
2023-02-24 21:13:09 +0100

    update
    w

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import styles from '../style';
+import { discount, robot } from '../assets';
+import GetStarted from './GetStarted';
+
+const Hero = () => (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+         <img src={discount} alt="discount" className="w-[32px] h-[32px] "/>
+         <p className={`${styles.paragraph} ml-2`}>
+         Get your {" "}
+         <span className="text-white">FREE</span> 15m {" "}
+         <span className="text-white">Demo Call</span> Today!
+         </p>
+        </div>
+
+         <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+            The next <br className="sm:block hidden" /> {" "}
+            <span className="text-gradient">Generation</span> {" "}
+            Digital <br></br> Marketing Agency
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+           <GetStarted />
+          </div>
+         </div>
+
+      </div>
+    </section>
+  )
+
+export default Hero
